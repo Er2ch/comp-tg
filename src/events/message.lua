17f1727be192042a47ae5ef31e80d61664159584
Er2 <er2@dismail.de>
2022-02-25 12:54:26 +0300

    ???

--- src/events/message.lua
+++ src/events/message.lua
@@ -1,9 +1,23 @@
+local reg = {
+  {'эх', 'хуех'}, -- надоели эхать
+  {'мета', 'хуета'},
+  {'meta', 'xueta'},
+  {'цукерберг', 'цукерхуй'},
+  {'zuckerberg', 'zuckerhui'},
+  {'whatsapp', 'вадзад'},
+  {'TikTok', 'деградация'},
+  {'.*че%?*$', 'пиши ё, грамотей'},
+  {'.*чё%?*$', 'ничё'}
+}
+
 return function(C, api, msg)
-  -- надоели эхать
-  if msg.text
-  and msg.text:match '[Ээ]+[Хх]+' then
-    C.api:reply(msg, msg.text
-      :gsub('[Ээ]+[Хх]+', 'хуех')
-    ,nil)
+  if msg.text then
+    local t = msg.text
+    for _, v in pairs(reg) do
+      t = t:gsub(v[1], v[2])
+    end
+    if msg.text ~= t
+    then api:reply(msg, t)
+    end
   end
 end
